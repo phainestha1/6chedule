f9df249bc2f8f02f09077c3c1808b92193509428
Hyeonsang <250wons@gmail.com>
2021-10-31 12:59:57 +0900

    initial commit

--- screens/Todo.js
+++ screens/Todo.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Alert, StyleSheet, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import styled from "styled-components/native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Feather } from "@expo/vector-icons";
 
 const STORAGE_KEY = "@toDos";
 
 export default function Todo() {
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
+  const [done, setDone] = useState(false);
 
   useEffect(async () => {
     loadToDos();
@@ -18,10 +20,15 @@ export default function Todo() {
     await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
   };
   const loadToDos = async () => {
+    const hours = new Date().getHours();
     try {
-      const s = await AsyncStorage.getItem(STORAGE_KEY);
-      if (s) {
-        setToDos(JSON.parse(s));
+      const getWorks = await AsyncStorage.getItem(STORAGE_KEY);
+      if (getWorks) {
+        if (String(hours) === "00") {
+          await AsyncStorage.removeItem(STORAGE_KEY);
+          return setToDos({});
+        }
+        setToDos(JSON.parse(getWorks));
       }
     } catch (error) {
       console.log(error);
@@ -33,10 +40,22 @@ export default function Todo() {
     if (text === "") {
       return;
     }
-    const newToDos = { ...toDos, [Date.now()]: { text } };
+    const newToDos = { ...toDos, [Date.now()]: { text, done } };
+    const toDoArr = Object.keys(newToDos);
+    if (toDoArr.length >= 7) {
+      setText("");
+      return Alert.alert("하루 6가지 일만 등록할 수 있어요!");
+    }
     setToDos(newToDos);
     await saveTodos(newToDos);
     setText("");
+  };
+
+  const handleDone = async (key) => {
+    const newToDos = { ...toDos };
+    newToDos[key].done = !newToDos[key].done;
+    setToDos(newToDos);
+    await saveTodos(newToDos);
   };
 
   return (
@@ -54,6 +73,13 @@ export default function Todo() {
       <TodoContainer>
         {Object.keys(toDos).map((key) => (
           <ToDoBox key={key}>
+            <Btn onPress={() => handleDone(key)}>
+              <Feather
+                name="check"
+                size={24}
+                color={toDos[key].done ? "red" : "black"}
+              />
+            </Btn>
             <TodoList>{toDos[key].text}</TodoList>
           </ToDoBox>
         ))}
@@ -78,13 +104,13 @@ const InputContainer = styled.View`
   justify-content: center;
   background-color: teal;
 `;
-
 const TodoContainer = styled.View`
   flex: 10;
   align-items: center;
   margin-top: 5%;
 `;
 const ToDoBox = styled.View`
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   width: 70%;
@@ -94,3 +120,4 @@ const ToDoBox = styled.View`
 const TodoList = styled.Text`
   font-size: 16px;
 `;
+const Btn = styled.TouchableOpacity``;
